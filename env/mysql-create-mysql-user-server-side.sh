ec439054265c8a45f2f4d052fcf232cfb67db1af
Vladyslav Gula <vladyslav.gula@gmail.com>
2014-12-07 17:16:02 +0200

    back

--- env/mysql-create-mysql-user-server-side.sh
+++ env/mysql-create-mysql-user-server-side.sh
@@ -6,6 +6,40 @@
 
 set -u
 
+ROOT_ACCESS=' -u root '
+
+function prepare_access {
+    mysql -u root -e "DROP USER '${USER_SOURCE}'@'%'"
+    mysql -u root -e "DROP USER '${USER_SOURCE}'@'localhost'"
+    mysql -u root -e "DROP USER '${USER_TARGET}'@'%'"
+    mysql -u root -e "DROP USER '${USER_TARGET}'@'localhost'"
+
+    mysql -u root -e "CREATE USER '${USER_SOURCE}'@'%'         IDENTIFIED BY '${USER_SOURCE_PASS}'"
+    mysql -u root -e "CREATE USER '${USER_SOURCE}'@'localhost' IDENTIFIED BY '${USER_SOURCE_PASS}'"
+    mysql -u root -e "CREATE USER '${USER_TARGET}'@'%'         IDENTIFIED BY '${USER_TARGET_PASS}'"
+    mysql -u root -e "CREATE USER '${USER_TARGET}'@'localhost' IDENTIFIED BY '${USER_TARGET_PASS}'"
+
+    mysql -u root -e "REVOKE ALL PRIVILEGES ON ${SOURCE_DB}.* FROM ${USER_SOURCE}"
+    mysql -u root -e "REVOKE GRANT OPTION   ON ${SOURCE_DB}.* FROM ${USER_SOURCE}"
+    mysql -u root -e "REVOKE ALL PRIVILEGES ON ${TARGET_DB}.* FROM ${USER_TARGET}"
+    mysql -u root -e "REVOKE GRANT OPTION   ON ${TARGET_DB}.* FROM ${USER_TARGET}"
+
+    echo 'Users:'
+    echo -e \\t ${USER_SOURCE}:
+    mysql -u root -e "GRANT Select, Lock Tables ON ${SOURCE_DB}.* TO '${USER_SOURCE}'@'%';"
+    mysql -u root -e "GRANT Select, Lock Tables ON ${SOURCE_DB}.* TO '${USER_SOURCE}'@'localhost';"
+    echo -e \\t ${USER_TARGET}:
+    mysql -u root -e "GRANT ALL PRIVILEGES  ON ${TARGET_DB}.* TO '${USER_TARGET}'@'%';"
+    mysql -u root -e "GRANT ALL PRIVILEGES  ON ${TARGET_DB}.* TO '${USER_TARGET}'@'localhost';"
+
+    mysql -u root -e "DROP USER '${USER_SOURCE}'@'%'"
+    mysql -u root -e "DROP USER '${USER_SOURCE}'@'localhost'"
+    mysql -u root -e "DROP USER '${USER_TARGET}'@'%'"
+    mysql -u root -e "DROP USER '${USER_TARGET}'@'localhost'"
+
+}
+
+
 USER_SOURCE='av2source' 
 USER_SOURCE_PASS='avS12'
 SOURCE_DB='a1_kiev'
@@ -14,26 +48,5 @@ USER_TARGET='av2target'
 USER_TARGET_PASS='avT14'
 TARGET_DB='av2_kiev'
 
-mysql -u root -e "DROP USER '${USER_SOURCE}'@'%'"
-mysql -u root -e "DROP USER '${USER_SOURCE}'@'localhost'"
-mysql -u root -e "DROP USER '${USER_TARGET}'@'%'"
-mysql -u root -e "DROP USER '${USER_TARGET}'@'localhost'"
-
-mysql -u root -e "CREATE USER '${USER_SOURCE}'@'%'         IDENTIFIED BY '${USER_SOURCE_PASS}'"
-mysql -u root -e "CREATE USER '${USER_SOURCE}'@'localhost' IDENTIFIED BY '${USER_SOURCE_PASS}'"
-mysql -u root -e "CREATE USER '${USER_TARGET}'@'%'         IDENTIFIED BY '${USER_TARGET_PASS}'"
-mysql -u root -e "CREATE USER '${USER_TARGET}'@'localhost' IDENTIFIED BY '${USER_TARGET_PASS}'"
-
-mysql -u root -e "REVOKE ALL PRIVILEGES ON ${SOURCE_DB}.* FROM ${USER_SOURCE}"
-mysql -u root -e "REVOKE GRANT OPTION   ON ${SOURCE_DB}.* FROM ${USER_SOURCE}"
-mysql -u root -e "REVOKE ALL PRIVILEGES ON ${TARGET_DB}.* FROM ${USER_TARGET}"
-mysql -u root -e "REVOKE GRANT OPTION   ON ${TARGET_DB}.* FROM ${USER_TARGET}"
-
-echo 'Users:'
-echo -e \\t ${USER_SOURCE}:
-mysql -u root -e "GRANT Select, Lock Tables ON ${SOURCE_DB}.* TO '${USER_SOURCE}'@'%';"
-mysql -u root -e "GRANT Select, Lock Tables ON ${SOURCE_DB}.* TO '${USER_SOURCE}'@'localhost';"
-echo -e \\t ${USER_TARGET}:
-mysql -u root -e "GRANT ALL PRIVILEGES  ON ${TARGET_DB}.* TO '${USER_TARGET}'@'%';"
-mysql -u root -e "GRANT ALL PRIVILEGES  ON ${TARGET_DB}.* TO '${USER_TARGET}'@'localhost';"
+prepare_access
 
